698beb631444ff35208660622b13547ae5fc4594
Iain Nash <info@codejoust.com>
2013-10-12 12:03:50 -0700

    Update to jinja

--- USCFreeandForSale/uscfreeandforsale.py
+++ USCFreeandForSale/uscfreeandforsale.py
@@ -1,9 +1,20 @@
+import os
+import urllib
 import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+  loader = jinja2.FileSystemLoader(os.path.dirname(__file__) + '/views'),
+  extensions = ['jinja2.ext.autoescape'])
+
+
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
 
 
 application = webapp2.WSGIApplication([
